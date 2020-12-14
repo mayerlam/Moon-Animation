ebf300f1cb977e8069fb7d5b839f73fd10b91de0
mayerlam <mayerdev01@gmail.com>
2020-12-14 13:01:38 +0800

    添加眼睛

--- Moon-Animation/bigMoon/bgView/BigFaceView.swift
+++ Moon-Animation/bigMoon/bgView/BigFaceView.swift
@@ -8,13 +8,41 @@
 import SwiftUI
 
 struct BigFaceView: View {
+    
+//    @GestureState private var dragOffset = CGSize.zero
+    @State private var position = CGSize.zero
+    
+    private var Eye: some View = {
+        Circle()
+            .foregroundColor(Color.black)
+    } ()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geo in
+            let scale = min(geo.size.width, geo.size.height) / 270
+            
+            ZStack(alignment: .topLeading) {
+                Eye
+                    .frame(width: size * scale, height: size * scale)
+                    .scaleEffect(0.7)
+                    .offset(x: leftEye.0 * scale , y: leftEye.1 * scale)
+                Eye
+                    .frame(width: size * scale, height: size * scale)
+                    .offset(x: rightEye.0 * scale , y: rightEye.1 * scale)
+            }
+        }
     }
+    
+    private let leftEye: (CGFloat, CGFloat) = (61, 133.62)
+    private let rightEye: (CGFloat, CGFloat) = (127, 132.62)
+    private let size: CGFloat = 19
 }
 
 struct BigFaceView_Previews: PreviewProvider {
     static var previews: some View {
         BigFaceView()
+            .previewLayout(.fixed(width: 270, height: 270))
+        BigFaceView()
+            .previewLayout(.fixed(width: 400, height: 400))
     }
 }
